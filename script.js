7bf3d867c96b844dd9038432c96797517a8e0fbf
julien-alaria <julienalaria@gmail.com>
2025-10-15 22:34:44 +0200

    implements modal

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+const cards = ["js", "php", "html", "css", "mysql", "sqlite", "figma", "git"];
 const sidebarBtn = document.querySelector(".sidebar-button");
 const aside = document.querySelector("aside");
+const modalBtn = document.querySelector(".overlay-button");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-button");
+const displayArray = document.querySelector(".display-array");
+
 
 sidebarBtn.addEventListener("click", function() {
     aside.classList.add("open");
-
 })
 
 aside.addEventListener("click", function() {
     aside.classList.remove("open");
 })
+
+modalBtn.addEventListener("click", function() {
+    overlay.style.display = "flex";
+})
+
+closeBtn.addEventListener("click", function() {
+    overlay.style.display = "none";
+})
+
+cards.forEach(cards => displayArray.innerText = cards);
+cards.forEach(cards => console.log(cards));
