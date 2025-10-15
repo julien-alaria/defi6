69b2707738eadce96f1e91a0871a93b1b3d4c0b3
julien-alaria <julienalaria@gmail.com>
2025-10-15 22:54:05 +0200

    forEach implements

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const modalBtn = document.querySelector(".overlay-button");
 const overlay = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close-button");
 const displayArray = document.querySelector(".display-array");
+const ulElement = document.querySelector("ul");
 
 
 sidebarBtn.addEventListener("click", function() {
@@ -23,5 +24,10 @@ closeBtn.addEventListener("click", function() {
     overlay.style.display = "none";
 })
 
-cards.forEach(cards => displayArray.innerText = cards);
-cards.forEach(cards => console.log(cards));
+cards.forEach((card) => {
+    const liElement = document.createElement("li");
+    liElement.innerText = card;
+    ulElement.appendChild(liElement);
+});
+
+cards.forEach(cards => console.log(cards))
