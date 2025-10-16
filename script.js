fccc4e8585129cc015e0b3e961e03b4874036877
julien-alaria <julienalaria@gmail.com>
2025-10-16 08:36:43 +0200

    topButton display test

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const cards = ["js", "php", "html", "css", "mysql", "sqlite", "figma", "git"];
-const sidebarBtn = document.querySelector(".sidebar-button");
-const aside = document.querySelector("aside");
-const modalBtn = document.querySelector(".overlay-button");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".close-button");
-const displayArray = document.querySelector(".display-array");
-const ulElement = document.querySelector("ul");
+const cards = ["js", "php", "html", "css", "mysql", "sqlite", "figma", "git", "test", "test", "test", "test","test", "test", "test", "test","test", "test", "test", "test","test", "test", "test", "test","test", "test", "test", "test","test", "test", "test", "test","test", "test", "test", "test","test", "test", "test", "test","test", "test", "test","test", "test","test", "test", "test", "test","test", "test", "test",];
 
+// Display SideBar
+
+const sidebarBtn = document.querySelector(".sidebar-button");
+const aside = document.querySelector(".sideBar");
 
 sidebarBtn.addEventListener("click", function() {
     aside.classList.add("open");
@@ -16,6 +13,12 @@ aside.addEventListener("click", function() {
     aside.classList.remove("open");
 })
 
+// Display Modal
+
+const modalBtn = document.querySelector(".overlay-button");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-button");
+
 modalBtn.addEventListener("click", function() {
     overlay.style.display = "flex";
 })
@@ -24,6 +27,11 @@ closeBtn.addEventListener("click", function() {
     overlay.style.display = "none";
 })
 
+// Display Cards
+
+const displayArray = document.querySelector(".display-array");
+const ulElement = document.querySelector("ul");
+
 cards.forEach((card) => {
     const liElement = document.createElement("li");
     liElement.innerText = card;
@@ -31,3 +39,29 @@ cards.forEach((card) => {
 });
 
 cards.forEach(cards => console.log(cards))
+
+
+//Scroll
+
+const header = document.querySelector("header");
+
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+
+scrollToTopBtn.addEventListener("click", function() {
+    header.scrollIntoView({
+        behavior: "smooth"
+    })
+})
+
+//Apparition du scroll Button
+
+let iScrollPos = 0;
+window.scroll(function() {
+    let iCurscrollPos = this.scrollTop();
+
+    if (iCurscrollPos > iScrollPos) {
+        scrollToTopBtn.classList.remove("show")
+    } else {
+        scrollToTopBtn.classListe.add("show");
+    }
+})
